f9a00c77f30c4b0cc2743e903b7c9616ba263a32
Sergey Denisenko <SelickSD@Gmail.com>
2022-03-07 18:56:41 +0300

    Initial commit

---
+++ Navigation/TabBarController.swift
@@ -0,0 +1,56 @@
+//
+//  TabBarController.swift
+//  Navigation
+//
+//  Created by Сергей Денисенко on 07.03.2022.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+
+    private enum TabBarItem {
+        case feed
+        case profile
+
+        var title: String {
+            switch self {
+            case .feed:
+                return "Лента"
+            case .profile:
+                return "Профиль"
+            }
+        }
+        var image: UIImage? {
+            switch self {
+            case .feed:
+                return UIImage(systemName: "pencil")
+            case .profile:
+                return UIImage(systemName: "pencil")
+            }
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.setupTabBar()
+    }
+    private func setupTabBar() {
+
+        let items: [TabBarItem] = [.feed, .profile]
+
+        self.viewControllers = items.map({tabBarItem in
+            switch tabBarItem {
+            case .feed:
+                return ViewController()
+            case .profile:
+                return ProfileViewController()
+            }
+        })
+
+        self.viewControllers?.enumerated().forEach({(index, viewController) in
+            viewController.tabBarItem.title = items[index].title
+            viewController.tabBarItem.image = items[index].image
+        })
+    }
+}
