b3f19afa2d60e7dda9ee1e2aaeddd6ddefab8f11
Sergey Denisenko <SelickSD@Gmail.com>
2022-03-12 00:59:17 +0300

    Добавлена функция скрывания клавиатуры

--- Profile/ProfileViewController.swift
+++ Profile/ProfileViewController.swift
@@ -15,10 +15,18 @@ class ProfileViewController: UIViewController {
         super.viewDidLoad()
         self.view.backgroundColor = .lightGray
         self.view.addSubview(profileHeaderView)
+        addTapGestureToHideKeyboard()
     }
 
     override func viewWillLayoutSubviews() {
         super.viewWillLayoutSubviews()
         profileHeaderView.frame = self.view.bounds
+    }
+}
+
+extension UIViewController {
+    func addTapGestureToHideKeyboard() {
+        let tapGesture = UITapGestureRecognizer(target: view, action: #selector(view.endEditing))
+        view.addGestureRecognizer(tapGesture)
     }
 }
